716d080a7454548916c9b8386c69b2aaa875ea0b
Luis Gomez <luis.gomezalonso@sicpa.com>
2021-06-02 16:57:00 +0200

    Allow external configuration. setup method overloaded
    Signed-off-by: Luis Gomez <luis.gomezalonso@sicpa.com>

--- http_uniresolver/__init__.py
+++ http_uniresolver/__init__.py
@@ -2,12 +2,27 @@
 
 from aries_cloudagent.config.injection_context import InjectionContext
 from aries_cloudagent.resolver.did_resolver_registry import DIDResolverRegistry
-
+import yaml
+import os
 from .http_universal import HTTPUniversalDIDResolver
 
+CONFIG_FILE = "aries-acapy-plugin-http-uniresolver/http_uniresolver/default_config.yml"
 
-async def setup(context: InjectionContext):
+
+async def setup(context: InjectionContext, endpoint: str = None, methods: list = None):
     """Setup the plugin."""
+    # Load config
+    if endpoint or methods:
+        with open(CONFIG_FILE, 'r') as default_config:
+            config = yaml.safe_load(default_config)
+        if endpoint:
+            config["endpoint"] = endpoint
+        if methods:
+            config["methods"] = methods
+        with open(CONFIG_FILE, 'w') as default_config:
+            default_config.write(yaml.safe_dump(config))
+
+    # Inject plugin
     registry = context.inject(DIDResolverRegistry)
     resolver = HTTPUniversalDIDResolver()
     await resolver.setup(context)
